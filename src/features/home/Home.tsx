6415d2346c51161ab0fdb0f1b97624625431c6e5
avtr-anthony <242517044+avtr-anthony@users.noreply.github.com>
2025-11-10 13:54:08 -0300

    feature(home): Estructura principal lista

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,8 +1,25 @@
 import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <div>
-      <Header></Header>
+    <div className="flex flex-col flex-1 min-h-0 ">
+      <Header
+        buttons={[
+          { label: "Inicio Sesión", href: "/login", variant: "outline" },
+          { label: "Registrarse", href: "/register", variant: "primary" },
+        ]}
+      ></Header>
+      <div className="flex flex-1 flex-col justify-center items-center">
+        <div className="max-w-3xl text-center flex-col gap-5 flex">
+          <p className="italic text-xl">
+            Construir hábitos saludables no se trata de cambiar de un día para
+            otro, sino de avanzar paso a paso hacia tu mejor versión
+          </p>
+          <h1 className="text-6xl font-black uppercase">
+            Se la mejor version de ti
+          </h1>
+        </div>
+      </div>
     </div>
   );
 }
