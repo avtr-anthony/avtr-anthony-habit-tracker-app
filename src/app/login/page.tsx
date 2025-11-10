a18b710c1c4679be2ad56d6e98fc0f35839fe00b
avtr-anthony <242517044+avtr-anthony@users.noreply.github.com>
2025-11-10 15:31:54 -0300

    Estructura login

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Header";
+
+export default function Login() {
+  return (
+    <div className="flex flex-col flex-1 min-h-0">
+      <Header />
+      <div className="flex flex-1 flex-col justify-center items-center">
+        <h1>falta card aqui</h1>
+      </div>
+    </div>
+  );
+}
