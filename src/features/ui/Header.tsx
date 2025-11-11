7b1f7aa052fb8144de28142cae0ab9c1a663e383
avtr-anthony <242517044+avtr-anthony@users.noreply.github.com>
2025-11-11 09:53:24 -0300

    feature(header): Se organizaron los botones y modifico tamaño logo responsive

--- src/features/ui/Header.tsx
+++ src/features/ui/Header.tsx
@@ -6,29 +6,26 @@ interface HeaderProps {
 }
 
 export default function Header({ buttons = [] }: HeaderProps) {
-  const styles = {
-    headerStyle: "",
-  };
-
   return (
-    <header className="bg-primary p-4 flex justify-between items-center text-white">
-      <Logo />
-
-      <nav className="flex gap-3">
-        {buttons.map((btn, i) => (
-          <Link
-            key={i}
-            href={btn.href}
-            className={
-              btn.variant === "outline"
-                ? "border border-white px-4 py-1 rounded-lg hover:bg-surface hover:text-primary transition"
-                : "bg-surface text-primary px-4 py-1 rounded-lg hover:opacity-90 transition"
-            }
-          >
-            {btn.label}
-          </Link>
-        ))}
-      </nav>
+    <header className="bg-primary text-white w-full">
+      <div className=" flex flex-wrap items-center justify-between gap-3 p-4 sm:px-6 md:px-10">
+        <Logo />
+        <nav className="flex flex-wrap justify-start sm:justify-end gap-2 sm:gap-3 w-full sm:w-auto">
+          {buttons.map((btn, i) => (
+            <Link
+              key={i}
+              href={btn.href}
+              className={
+                btn.variant === "outline"
+                  ? "border border-white px-4 py-1 rounded-lg hover:bg-surface hover:text-primary transition text-sm sm:text-base"
+                  : "bg-surface text-primary px-4 py-1 rounded-lg hover:opacity-90 transition text-sm sm:text-base"
+              }
+            >
+              {btn.label}
+            </Link>
+          ))}
+        </nav>
+      </div>
     </header>
   );
 }
