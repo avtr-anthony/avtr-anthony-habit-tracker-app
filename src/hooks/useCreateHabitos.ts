d4a80dab1f61f32655cd8ac88988ad29a750565c
avtr-anthony <242517044+avtr-anthony@users.noreply.github.com>
2025-11-13 16:06:38 -0300

    feature/hook-serive-habitos: Manejo de habitos services

---
+++ src/hooks/useCreateHabitos.ts
@@ -0,0 +1,52 @@
+"use client";
+import { createHabito } from "@/services/habitosService";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export function useCreateHabito() {
+  const router = useRouter();
+
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleCreate(ev: React.FormEvent<HTMLFormElement>) {
+    ev.preventDefault();
+    setLoading(true);
+
+    const form = new FormData(ev.currentTarget);
+
+    const descripcion = String(form.get("descripcion") || "");
+    const label = String(form.get("label") || "");
+    const fecha = String(form.get("fecha") || "");
+    const hora = String(form.get("hora") || "");
+
+    if (!label || !fecha || !hora) {
+      setError("Completa los campos");
+      setTimeout(() => {
+        setLoading(false);
+      }, 300);
+      return;
+    }
+
+    try {
+      await createHabito({
+        descripcion,
+        label,
+        fecha,
+        hora
+      });
+
+      router.push("/habitos");
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError("Error desconocido al crear habito");
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return { handleCreate, error, loading };
+}
