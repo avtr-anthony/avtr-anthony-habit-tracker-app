618c8db2c4aa5d40a59927052f119ada830b7478
avtr-anthony <242517044+avtr-anthony@users.noreply.github.com>
2025-11-17 17:43:48 -0300

    cambio minimo container

--- src/features/ui/Container.tsx
+++ src/features/ui/Container.tsx
@@ -10,7 +10,7 @@ interface ContainerProps {
 
 export default function Container({ children, variant = "default" }: ContainerProps) {
   const containerClasses = clsx(
-    "w-full  h-auto  flex flex-1 bg-linear-to-br from-slate-200 via-slate-400 to-slate-500 md:overflow-hidden",
+    "w-full  h-auto  flex flex-1 bg-linear-to-br from-slate-200 via-slate-200 to-slate-400 md:overflow-hidden",
     {
       "items-start justify-start p-0 bg-surface  ": variant === "panel",
       "flex-col  items-center justify-center px-6 py-10 ": variant === "default"
